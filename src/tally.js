e82cecce07498af2fceeaaa0a82296f24d3486be
Zoooook <zzzzzzooooook@gmail.com>
2016-11-04 13:35:59 -0600

    tally function done! Passes back an array of winners

--- src/tally.js
+++ src/tally.js
@@ -34,7 +34,7 @@ function tallyResults(election_id, done){
     }
 
     Object.keys(votes).forEach(function(key, index){
-      for(var i=0; i<matrix_map.length; ++i){
+      for(var i=0; i<matrix_map.length-1; ++i){
         for(var j=i+1; j<matrix_map.length; ++j){
           pos_i = votes[key].indexOf(matrix_map[i]);
           pos_j = votes[key].indexOf(matrix_map[j]);
@@ -54,21 +54,54 @@ function tallyResults(election_id, done){
       }
     });
 
-    done(pair_matrix);
+    var path_matrix = [];
+    for(var i=0; i<matrix_map.length; ++i){
+      var matrix_row = [];
+      for(var j=0; j<matrix_map.length; ++j)
+        matrix_row.push(pair_matrix[i][j]);
+      path_matrix.push(matrix_row);
+    }
+
+    for(var i=0; i<matrix_map.length; ++i)
+      for(var j=0; j<matrix_map.length; ++j)
+        if(i!=j)
+          for(var k=0; k<matrix_map.length; ++k)
+            if(i!=k && j!=k)
+              path_matrix[j][k] = Math.max(path_matrix[j][k], Math.min(path_matrix[j][i], path_matrix[i][k]));
+
+    var wins = [];
+    for(var i=0; i<matrix_map.length; ++i)
+      wins.push(0);
+
+    for(var i=0; i<matrix_map.length-1; ++i)
+      for(var j=i+1; j<matrix_map.length; ++j)
+        if(path_matrix[i][j] > path_matrix[j][i])
+          wins[i]++;
+        else if(path_matrix[i][j] < path_matrix[j][i])
+          wins[j]++;
+
+    var max_wins = Math.max.apply(null, wins);
+
+    var winners = [];
+    for(var i=0; i<wins.length; ++i)
+      if(wins[i] == max_wins)
+        winners.push(matrix_map[i]);
+
+    done(winners);
   })
 }
 
 db.serialize(function() {
-  db.run("create table votes (id int PRIMARY KEY NOT NULL, election_id int NOT NULL, voter_id int NOT NULL, option_id int NOT NULL, rank tinyint NOT NULL)");
-  db.run("INSERT INTO votes (id, election_id, voter_id, option_id, rank) VALUES (1, 1, 1, 6, 3)");
-  db.run("INSERT INTO votes (id, election_id, voter_id, option_id, rank) VALUES (2, 1, 1, 4, 1)");
-  db.run("INSERT INTO votes (id, election_id, voter_id, option_id, rank) VALUES (3, 1, 2, 6, 2)");
-  db.run("INSERT INTO votes (id, election_id, voter_id, option_id, rank) VALUES (4, 1, 1, 5, 2)");
-  db.run("INSERT INTO votes (id, election_id, voter_id, option_id, rank) VALUES (5, 1, 2, 4, 1)");
-  db.run("INSERT INTO votes (id, election_id, voter_id, option_id, rank) VALUES (6, 1, 3, 5, 2)");
-  db.run("INSERT INTO votes (id, election_id, voter_id, option_id, rank) VALUES (7, 1, 2, 5, 3)");
-  db.run("INSERT INTO votes (id, election_id, voter_id, option_id, rank) VALUES (8, 1, 3, 6, 1)");
-  db.run("INSERT INTO votes (id, election_id, voter_id, option_id, rank) VALUES (9, 1, 3, 4, 3)");
+  db.run("create table votes (id integer PRIMARY KEY NOT NULL, election_id int NOT NULL, voter_id int NOT NULL, option_id int NOT NULL, rank tinyint NOT NULL)");
+  db.run("INSERT INTO votes (election_id, voter_id, option_id, rank) VALUES (1, 1, 6, 3)");
+  db.run("INSERT INTO votes (election_id, voter_id, option_id, rank) VALUES (1, 1, 4, 1)");
+  db.run("INSERT INTO votes (election_id, voter_id, option_id, rank) VALUES (1, 2, 6, 2)");
+  db.run("INSERT INTO votes (election_id, voter_id, option_id, rank) VALUES (1, 1, 5, 2)");
+  db.run("INSERT INTO votes (election_id, voter_id, option_id, rank) VALUES (1, 2, 4, 1)");
+  db.run("INSERT INTO votes (election_id, voter_id, option_id, rank) VALUES (1, 3, 5, 2)");
+  db.run("INSERT INTO votes (election_id, voter_id, option_id, rank) VALUES (1, 2, 5, 3)");
+  db.run("INSERT INTO votes (election_id, voter_id, option_id, rank) VALUES (1, 3, 6, 1)");
+  db.run("INSERT INTO votes (election_id, voter_id, option_id, rank) VALUES (1, 3, 4, 3)");
 
   tallyResults(1, function(output){
     console.log(output);
