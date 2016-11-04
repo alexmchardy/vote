ed2faab58c21e4c6cd17a2dca083ea59b88099bc
Zoooook <zzzzzzooooook@gmail.com>
2016-11-03 19:23:46 -0600

    tally counts number of wins of each candidate vs each other candidate

--- src/tally.js
+++ src/tally.js
@@ -3,6 +3,7 @@ var db = new sqlite3.Database(':memory:');
 
 function tallyResults(election_id, done){
   var votes = {};
+  options = new Set();
 
   db.each("SELECT voter_id, option_id, rank FROM votes WHERE election_id = " + election_id, function(err, row){
     if(!votes[row.voter_id])
@@ -17,11 +18,43 @@ function tallyResults(election_id, done){
       });
 
       votes[key] = votes[key].map(function(obj){
+        options.add(obj.option_id);
         return obj.option_id;
       });
     });
 
-    done(votes);
+    var matrix_map = Array.from(options);
+
+    var pair_matrix = [];
+    for(var i=0; i<matrix_map.length; ++i){
+      var matrix_row = [];
+      for(var j=0; j<matrix_map.length; ++j)
+        matrix_row.push(0);
+      pair_matrix.push(matrix_row);
+    }
+
+    Object.keys(votes).forEach(function(key, index){
+      for(var i=0; i<matrix_map.length; ++i){
+        for(var j=i+1; j<matrix_map.length; ++j){
+          pos_i = votes[key].indexOf(matrix_map[i]);
+          pos_j = votes[key].indexOf(matrix_map[j]);
+
+          if(pos_i == -1){
+            if(pos_j != -1)
+              pair_matrix[j][i] += 1;
+          }else{
+            if(pos_j == -1)
+              pair_matrix[i][j] += 1;
+            else if(pos_i < pos_j)
+              pair_matrix[i][j] += 1;
+            else
+              pair_matrix[j][i] += 1;
+          }
+        }
+      }
+    });
+
+    done(pair_matrix);
   })
 }
 
@@ -37,8 +70,8 @@ db.serialize(function() {
   db.run("INSERT INTO votes (id, election_id, voter_id, option_id, rank) VALUES (8, 1, 3, 6, 1)");
   db.run("INSERT INTO votes (id, election_id, voter_id, option_id, rank) VALUES (9, 1, 3, 4, 3)");
 
-  tallyResults(1, function(votes){
-    console.log('votes', votes);
+  tallyResults(1, function(output){
+    console.log(output);
   });
 });
 
