34a8290ff9cd7aab29dc0ff2a2059401b4d5b91e
Jesse Weaver <jweaver@shutterstock.com>
2016-11-04 11:59:06 -0600

    Updating sql. Prepopulating db with some data

--- data/voter_database.sql
+++ data/voter_database.sql
@@ -34,10 +34,23 @@ create table votes (
 );
 
 /* Populate Data */
-insert into election_voter_options(election_id, voter_options_id) values (1, 3);
-insert into election_voter_options(election_id, voter_options_id) values (1, 3);
-insert into election_voter_options(election_id, voter_options_id) values (1, 3);
-insert into election_voter_options(election_id, voter_options_id) values (1, 3);
-insert into election_voter_options(election_id, voter_options_id) values (1, 3);
+insert into voters (name, email, enabled) values
+    ('Alex McHardy', 'amchardy@shutterstock.com', 1),
+    ('Jesse Weaver', 'jweaver@shutterstock.com', 1),
+    ('Daniel Berry', 'dberry@shutterstock.com', 1),
+    ('Nick Winn', 'nwinn@shutterstock.com', 1);
 
+insert into elections (name, description) values ('Lunch Vote', 'Friday, November 11th');
+
+insert into options (name, description) values 
+    ('Lucilles', 'Best cajun food this side of the missisippi'),
+    ('Yazoo BBQ', 'BBQ to blow your mind'),
+    ('Teds Cheesesteaks', 'Heartburn for days'),
+    ('Maggianos', 'always have a contingency plan');
+
+
+insert into election_option_map(election_id, option_id) values (1, 1);
+insert into election_option_map(election_id, option_id) values (1, 2);
+insert into election_option_map(election_id, option_id) values (1, 3);
+insert into election_option_map(election_id, option_id) values (1, 4);
 
